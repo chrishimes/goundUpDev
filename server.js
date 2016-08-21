bde5d5f34ff48f27946166c51cb8ac5a722ffb25
chrishimes <chris@chrishimes.com>
2016-08-20 22:46:53 -0600

    Working through error when using strict

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-'use strict';
+//'use strict';
 
-//Require items to start the process
+/*/Require items to start the process
 var http = require('http');
 var path = require('path');
 var url = require("url");
 var express = require('express');
+*/
+
+
+
+//Newest requirements to correct the old parsing error
+var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+
+
+
 
 //mongo connection setup
 var databaseUrl =  "mongodb://localhost:27017/MLSecation";
@@ -16,7 +32,7 @@ var ObjectId = mongojs.ObjectId;
 var fs=require('fs') ;
 var crypto = require('crypto');
 
-//this is the incoming router request which send everything to the client folder 
+/*//this is the incoming router request which send everything to the client folder
 // needed to install npm install body-parser --save for this work
 var router = express();
 var server = http.createServer(router);
@@ -24,3 +40,38 @@ router.use(express.bodyParser());
 router.use(express.static(path.resolve(__dirname, 'client')));
 // do know yet router.use( express.cookieParser() );
 // do know yet router.use(express.session({secret: '1234567890QWERTY'}));
+*/
+
+var app = express();
+//var server = http.createServer(router);
+app.set('port', process.env.PORT || 80, process.env.IP || "0.0.0.0", function(){
+    var addr = server.address();
+    console.log("server listening at", addr.address + ":" + addr.port)
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+// not sure what this is for app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ resave: true, saveUninitialized: true,
+    secret: 'uwotm8' }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse multipart/form-data
+app.use(multer());
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.listen(app.get('port'), function(){
+    console.log('Express server on port ' + app.get('port'));
+});
+
+/*server.listen(process.env.PORT || 80, process.env.IP || "0.0.0.0", function(){
+    var addr = server.address();
+    console.log("server listening at", addr.address + ":" + addr.port);  */
